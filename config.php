9f99c1e8aa5ec56bc1e9b3d0786ce73b50a01d5f
Sankalp <sankalp@sportskeeda.com>
2015-10-08 00:20:36 +0530

    Added config file

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define("DB_HOSTNAME", "localhost");
+define("DB_PORT", "3307");
+define("DB_NAME", "database_name");
+define("DB_USERNAME", "username");
+define("DB_PASSWORD", "password");
+
+
